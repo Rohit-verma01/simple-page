a570982ca9dd7095a4a12ca1cb17d09f66112090
Rohit Verma <rohit.verma@intimetec.com>
2023-09-21 14:01:34 +0530

    revert the changes

--- src/SharingPage/index.js
+++ src/SharingPage/index.js
@@ -4,10 +4,25 @@ import { Helmet } from "react-helmet";
 const SharingPage = () => {
   return (
     <div>
-      {/* <Helmet> */}
-      {/* Twitter Card meta tags */}
-
-      {/* </Helmet> */}
+      <Helmet>
+        {/* Twitter Card meta tags */}
+        <meta
+          property="og:image"
+          content="https://www.cameraegg.org/wp-content/uploads/2016/01/Nikon-D500-Sample-Images-7-600x400.jpg"
+        ></meta>
+        <meta property="og:image:width" content="640"></meta>
+        <meta property="og:image:height" content="144"></meta>
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Check awesome content" />
+        <meta
+          name="twitter:description"
+          content="It is the only one awesome content"
+        />
+        <meta
+          name="twitter:image"
+          content="https://www.cameraegg.org/wp-content/uploads/2016/01/Nikon-D500-Sample-Images-7-600x400.jpg"
+        />
+      </Helmet>
       {/* Your component content */}
 
       <img
